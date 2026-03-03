42e69b7d5a20e2c43a315fe1b243a0ef2325ae4a
Javier Andrés Betanzos Garcia <javierandresbetanzosgarcia@iMac-de-Javier.local>
2026-03-03 20:26:06 +0100

    Fix lockfile error in Actions (v1.0.14)

--- App.tsx
+++ App.tsx
@@ -466,7 +466,7 @@ const App = () => {
                     <span>Escanear Nuevo Documento</span>
                 </button>
                 <p className="text-[10px] text-slate-300 text-center mt-4">
-                    Versión: 1.0.13 - {new Date().toLocaleString()}
+                    Versión: 1.0.14 - {new Date().toLocaleString()}
                 </p>
             </div>
         </div>
